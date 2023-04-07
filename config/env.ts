1ac7e256ef5a2b9040940fbc756fd65665e893c0
Andrii Kogut <80691345+andrii-kogut-vitech@users.noreply.github.com>
2023-04-07 12:06:29 +0300

    Delete env.ts

--- config/env.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const env = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS || '',
-};
